30aa1054552718736e3ca15c6772f053f9116b7f
Michał Kundera <mychto6ff@gmail.com>
2018-06-23 16:31:17 +0200

    Login auth userExistException

--- src/main/java/paradiseTravels/bean/user/AuthBean.java
+++ src/main/java/paradiseTravels/bean/user/AuthBean.java
@@ -1,6 +1,7 @@
 package paradiseTravels.bean.user;
 
 import paradiseTravels.bean.user.exception.InvalidCredentialsException;
+import paradiseTravels.bean.user.exception.UserAlreadyLoggedException;
 import paradiseTravels.dao.UsersDAO;
 import paradiseTravels.model.User;
 
@@ -12,15 +13,29 @@ import javax.servlet.http.HttpSession;
 @RequestScoped
 public class AuthBean {
 
-//    @Inject
-//    UsersDAO usersDAO;
+    @Inject
+    UsersDAO usersDAO;
 
-    public void loginUser(HttpSession session, String login, String password) throws InvalidCredentialsException {
-//        User user = usersDAO.findByLoginPassword(login, password);
-//
-//        if(user == null) {
-//            throw new InvalidCredentialsException();
-//        }
-//        session.setAttribute("user", user);
+    public void loginUser(HttpSession session, String login, String password) throws InvalidCredentialsException,UserAlreadyLoggedException {
+        User user = usersDAO.findByLoginPassword(login, password);
+
+        if(user == null) {
+            throw new InvalidCredentialsException();
+        }
+        if(isAlreadyLogged(session))
+        {
+            throw new UserAlreadyLoggedException();
+        }
+
+        session.setAttribute("user", user);
+
+    }
+
+
+    private boolean isAlreadyLogged(HttpSession session)
+    {
+        User loggedUser = (User) session.getAttribute("user");
+        if(loggedUser == null) return false;
+        else return true;
     }
 }
